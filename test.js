ba7f94e9dbf1c41e5d6ff7d5dfe2093e61b9da1c
CosminBM <cosmincoco88@gmail.com>
2018-07-25 14:31:51 +0300

    Update1

--- test.js
+++ test.js
@@ -1,56 +1,76 @@
-https://eloquentjavascript.net/code#2
+//https://eloquentjavascript.net/code#2
+//
+//
+//2.1 Looping a triangle
+//
+//for (let n = "#"; n.length < 8; n+= "#")
+//    console.log(n);
+//
+//
+//2.2 FizzBuzz - complete
+
+//for (let n = 1; n <= 100; n = n + 1){
+//    if ((n % 3 == 0) && (n % 5 == 0) ){
+//       console.log("FizzBuzz");
+//    }else if (n % 3 == 0) {
+//        console.log("Fizz");
+//    }else if (n % 5 == 0 ){
+//        console.log("Buzz");
+//    }else {
+//        console.log(n);
+//    }  
+//}
+//
+//
+//2.3 Chess Board 
+//let size = 10;
+//let grid = "";
+//
+//for(i = 0; i < size; i++){
+//    for(j = 0; j < size; j++){
+//        if((i+j) %2 == 0){
+//            grid += " ";
+//        }else {
+//            grid += "#";
+//        }
+//    }
+//    grid += "\n";
+//}
+//console.log(grid);
 
 
-2.1 Looping a triangle
+//3.1 Minimum
 
-for (let n = "#"; n.length < 8; n+= "#")
-    console.log(n);
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
 
-
-2.2 FizzBuzz - complete
-
-for (let n = 1; n <= 100; n = n + 1){
-    if ((n % 3 == 0) && (n % 5 == 0) ){
-       console.log("FizzBuzz");
-    }else if (n % 3 == 0) {
-        console.log("Fizz");
-    }else if (n % 5 == 0 ){
-        console.log("Buzz");
-    }else {
-        console.log(n);
-    }  
-}
-
-
-2.3 Chess Board 
-
-3.1 Minimum
-
-3.2 Recursion
-
-3.3 Bean counting
-
-4.1The sum of a range.
-
-//In progress
-function range () {
-    let array = [];
-    
-    for (i = 1; i <= 10; i ++ ){
-            array.push(i);       
-        }  
-    
-    return array.reduce(); 
-}
-
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-//In progress
-
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-
-console.log(sum(range(1, 10)));
-// → 55
+//3.2 Recursion
+//
+//3.3 Bean counting
+//
+//4.1The sum of a range.
+//
+////In progress
+//function range () {
+//    let array = [];
+//    
+//    for (i = 1; i <= 10; i ++ ){
+//            array.push(i);       
+//        }  
+//    
+//    return array.reduce(); 
+//}
+//
+//console.log(range(1, 10));
+//// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//
+////In progress
+//
+//console.log(range(5, 2, -1));
+//// → [5, 4, 3, 2]
+//
+//console.log(sum(range(1, 10)));
+//// → 55
 
