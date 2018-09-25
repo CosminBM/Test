abeeb6dc54e9ece842321a93df1542ef97922128
CosminBM <cosmincoco88@gmail.com>
2018-09-25 18:01:24 +0300

    Updated

--- Udemy/Section2/scripts.js
+++ Udemy/Section2/scripts.js
@@ -2,6 +2,7 @@
 Variables and data types
 */
 
+/*
 var firstName = 'John';
 console.log(firstName);
 
@@ -21,8 +22,37 @@ console.log(job);
 var _4years = 5;
 var hahaHH = 'And So';
 var if = 23;
+*/
 
 /*******************
 Variable mutation and type coercion
 */
+
+// Type coercion
+var firstName = 'John';
+var age = 29;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = 'false';
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt(' What is his last Name? ');
+console.log(firstName + ' ' + lastName);
+
+
+
+
+
+
 
