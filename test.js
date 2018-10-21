9ca1266b5fc4e29feec9f3b33c4e97a209c07b10
CosminBM <cosmincoco88@gmail.com>
2018-10-21 17:09:09 +0300

    Asa

--- test.js
+++ test.js
@@ -68,23 +68,23 @@
 
 
 ////3.3 Bean counting
-function countChar(string, ch) {
-  let counted = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] == ch) {
-      counted += 1;
-    }
-  }
-  return counted;
-}
-
-function countBs(string) {
-  return countChar(string, "B");
-}
-
-console.log(countBs("BBC"));
-// → 2
-console.log(countChar("kakkerlak", "k"));
+//function countChar(string, ch) {
+//  let counted = 0;
+//  for (let i = 0; i < string.length; i++) {
+//    if (string[i] == ch) {
+//      counted += 1;
+//    }
+//  }
+//  return counted;
+//}
+//
+//function countBs(string) {
+//  return countChar(string, "B");
+//}
+//
+//console.log(countBs("BBC"));
+//// → 2
+//console.log(countChar("kakkerlak", "k"));
 // → 4
 
 //4.1The sum of a range.
