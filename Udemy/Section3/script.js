3c061c028ca889d03309cd3ee8a1ebd238f553b8
CosminBM <cosmincoco88@gmail.com>
2018-10-30 18:18:27 +0200

    33

--- Udemy/Section3/script.js
+++ Udemy/Section3/script.js
@@ -1,20 +1,36 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
 
+//Function hoisting
+//function declaration
+calculateAge(1965);
+
+function calculateAge(year) {
+    console.log(2016 - year);
+}
 
 
 
+//retirement(1956);
 
+//function expression
+var retirement = function(year) {
+    console.log(65 - (2016 - year));
+}
 
+//Variables hoisting
 
+console.log(age);
+var age = 23;
+console.log(age);
 
-
-
-
-
-
-
-
+function foo() {
+    console.log(age);
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
 
 ///////////////////////////////////////
@@ -23,7 +39,7 @@
 
 // First scoping example
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -36,13 +52,12 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
 var a = 'Hello!';
 first();
 
@@ -58,9 +73,10 @@ function first() {
 
 function third() {
     var d = 'John';
-    console.log(a + b + c + d);
+    console.log(a + d);
+    
 }
-*/
+
 
 
 
