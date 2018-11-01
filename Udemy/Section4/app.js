ef791ae6fc9bccd119098aa1ca1582f9260ed6cb
CosminBM <cosmincoco88@gmail.com>
2018-11-01 18:02:47 +0200

    123123

--- Udemy/Section4/app.js
+++ Udemy/Section4/app.js
@@ -9,4 +9,116 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function btn(){
+    
+    //1. Random number.
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    //2. Display the result.
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    //3. Update the round score IF the rolled number was NOT 1.
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        
+//        document.querySelector('.player-0-panel').classList.add('active');
+//        document.querySelector('.player-1-panel').classList.remove('active');
+        
+    } else {
+        //Next player
+        nextPlayer();
+        
+    }
+    
+});
+
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+   
+    
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    //Check if player won the game
+    for (let i = 0; i <= 10; i++) {
+        if (i == '#score-0') {
+            document.getElementById('#score-0').textContent = alert('#name-0' + activePlayer + ' has won the Game!');
+        } else {
+            document.getElementById('#score-1').textContent = alert('#name-1' + activePlayer + ' has won the Game!');
+        }
+    }
+    
+    
+    //Next player
+    nextPlayer();
+    
+    
+})
+
+
+
+function nextPlayer() {
+//Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;    
+//        Different method
+//        if(activePlayer === 0) { 
+//            activePlayer = 1;
+//        } else {
+//            activePlayer = 0;
+//        }
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active')
+        
+//        document.querySelector('.player-0-panel').classList.remove('active');
+//        document.querySelector('.player-1-panel').classList.add('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+        
+}
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log('The GLOBAL score for Player 1 is: ' + x);
+//
+//var x = document.querySelector('#score-1').textContent;
+//console.log('The GLOBAL score for Player 2 is: ' + x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
