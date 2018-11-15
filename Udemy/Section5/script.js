bdb5239cc2356e44649dbbfa88bc62b208c3700d
CosminBM <cosmincoco88@gmail.com>
2018-11-15 17:25:05 +0200

    23

--- Udemy/Section5/script.js
+++ Udemy/Section5/script.js
@@ -231,7 +231,7 @@
 //    var score = Math.random() * 10;
 //    console.log(score >= 5);
 //}
-//game();
+//game(3);
 //
 //(function () {
 //    var score = Math.random() * 10;
@@ -248,8 +248,105 @@
 //
 ///////////////////////////
 ////Lecture: Closures
-
-
+//
+//
+//function retirement(retirementAge) {
+//    var a = ' years left until retirement.';
+//    return function(yearOfBirth) {
+//        var age = 2016 - yearOfBirth;
+//        console.log((retirementAge - age) + a);
+//    }
+//}
+//
+//var retirementUS = retirement(66);
+////retirement(66)(1990);
+//var retirementGermany = retirement(65);
+//var retirementIceland = retirement(67);
+//
+//retirementGermany(1990);
+//retirementUS(1990);
+//retirementIceland(1990);
+//
+//
+//
+//function interviewQuestion(job) {
+//    return function(name) {
+//        if ( job === 'designer') {
+//            console.log(name + ', can you please explain what UX design is?');
+//        } else if ( job === 'teacher') {
+//            console.log('What subject do you teach, ' + name +'?');
+//        } else {
+//            console.log('Hello ' + name + ', what do you do?');
+//        } 
+//    }   
+//}
+//
+//interviewQuestion('designer')('John');
+//interviewQuestion('teacher')('Mark');
+//interviewQuestion('')('Jonas');
+//
+///////////////////////////
+////Lecture: Bind, call and apply
+//
+//var john = {
+//    name: 'John',
+//    age: 26,
+//    job: 'teacher',
+//    presentation: function(style, timeOfDay) {
+//        if (style === 'formal') {
+//            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ' I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+//        } else if (style === 'friendly') {
+//            console.log('Hey! What\'s up? I\'m ' + this.name + ' I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+//        }
+//    }  
+//}
+//
+//var emily = {
+//    name: 'Emily',
+//    age: 35,
+//    job: 'designer'
+//};
+//
+//
+//john.presentation('formal', 'morning');
+//
+//john.presentation.call(emily, 'friendly', 'afternoon');
+//
+////john.presentation.apply(emily, ['friendly', 'afternoon']);
+////Caryying
+//var johnFriendly = john.presentation.bind(john, 'friendly');
+//
+//johnFriendly('morning');
+//johnFriendly('night');
+//
+//
+//var emilyFormal = john.presentation.bind(emily, 'formal');
+//emilyFormal('afternoon');
+//
+//
+//var years = [1990, 1965, 1937, 2005, 1998];
+//
+//function arrayCalc(arr, fn) {
+//    var arrRes = [];
+//    for (var i = 0; i < arr.length; i++) {
+//        arrRes.push(fn(arr[i]));
+//    }
+//    return arrRes;
+//}
+//
+//function calculateAge(el) {
+//    return 2016 - el;
+//}
+//
+//function isFullAge(limit, el) {
+//    return el >= limit;
+//}
+//
+//
+//var ages = arrayCalc(years, calculateAge);
+//var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+//console.log(ages);
+//console.log(fullJapan);
 
 
 
