05ce65ac670e8a6c27e3a4f85a2c11a171d7b096
CosminBM <cosmincoco88@gmail.com>
2018-11-19 18:35:41 +0200

    32

--- Udemy/Section5/script.js
+++ Udemy/Section5/script.js
@@ -1,19 +1,19 @@
 /////////////////////////////
 // //Lecture: Function constructor
-//
-////Function constructor
-//
-//var john = {
-//    name: 'John',
-//    yearOfBirth: 1990,
-//    job: 'teacher'
-//};
+////
+//////Function constructor
+////
+////var john = {
+////    name: 'John',
+////    yearOfBirth: 1990,
+////    job: 'teacher'
+////};
 //
 //var Person = function(name, yearOfBirth, job) {
 //    this.name = name;
 //    this.yearOfBirth = yearOfBirth;
 //    this.job = job;
-// 
+//  
 //}
 //
 //Person.prototype.calculateAge = function() {
